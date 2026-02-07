a9ee7816924a7af57ba9257ae28c830e83055762
Ben Freijah <ben@launchkeylab.com>
2026-02-07 14:59:06 +1100

    feat: Using Class instead of constructor. - Turned other code into factory functions.

--- script.js
+++ script.js
@@ -36,97 +36,125 @@ const array = [
     }
 ];
 
-const table = document.querySelector("tbody");
-const form = document.querySelector("form");
-const bookBtn = document.getElementById("new_book");
-const popupElement = document.querySelector(".popup");
+class Book {
+    constructor(item) {
+        this.title = item.title;
+        this.content = item.content;
+        this.author = item.author;
+        this.pages = item.pages;
+        this.status = item.status;
+        this.UID = crypto.randomUUID();
+    }
 
-let myLibrary = [];
-
-bookBtn.addEventListener("click", () => {
-    popupElement.setAttribute("class", "popup open");
-});
-
-function Book(title, content, author, pages, status) {
-    this.title = title;
-    this.content = content;
-    this.author = author;
-    this.pages = pages;
-    this.status = status;
-    this.UID = crypto.randomUUID();
+    setStatus(newStatus) {
+        this.status = newStatus;
+    }
 };
 
-Book.prototype.setStatus = function (newStatus) {
-    this.status = newStatus;
-};
+const init = (() => {
+    const table = document.querySelector("tbody");
+    const form = document.querySelector("form");
+    const bookBtn = document.getElementById("new_book");
+    const popupElement = document.querySelector(".popup");
 
-function initialLoad() {
-    array.forEach(item => {
-        const book = new Book(item.title, item.content, item.author, item.pages, item.status);
-
-        addBookToLibrary(book);
+    bookBtn.addEventListener("click", () => {
+        buildDOM.showPopup();
     });
-};
 
-initialLoad();
-
-function addBookToLibrary(book) {
-    myLibrary.push(book);
-
-    const row = document.createElement("tr");
-    const cell1 = document.createElement("td");
-    const cell2 = document.createElement("td");
-    const cell3 = document.createElement("td");
-    const cell4 = document.createElement("td");
-    const cell5 = document.createElement("td");
-    const cell6 = document.createElement("td");
-
-    cell1.textContent = book.title;
-    cell2.textContent = book.content;
-    cell3.textContent = book.author;
-    cell4.textContent = book.pages;
-    cell5.innerHTML = `
-        <select data-id="${book.UID}">
-            <option value="">Select a value...</option>
-            <option value="Reading" ${book.status === "Reading" ? "selected" : ""}>Reading</option>
-            <option value="Completed" ${book.status === "Completed" ? "selected" : ""}>Completed</option>
-            <option value="Dropped" ${book.status === "Dropped" ? "selected" : ""}>Dropped</option>
-            <option value="Pending" ${book.status === "Pending" ? "selected" : ""}>Pending</option>
-        </select>
-    `;
-    cell6.innerHTML = `<button class='trash' id='${book.UID}'>🗑️</button>`;
-
-    table.appendChild(row);
-    row.append(cell1, cell2, cell3, cell4, cell5, cell6);
-}
-
-table.addEventListener("click", (e) => {
+    table.addEventListener("click", (e) => {
         const button = e.target.closest(".trash");
         if (!button) return;
-        
-        myLibrary = myLibrary.filter(book => book.UID !== button.id);
 
-        button.closest("tr").remove();
+        libraryController.myLibrary = libraryController.myLibrary.filter(book => book.UID !== button.id);
+
+        console.log(libraryController.myLibrary);
+        buildDOM.removeRow(button);
     });
 
-table.addEventListener("change", (e) => {
-    const select = e.target.closest("select");
-    if (!select) return;
+    table.addEventListener("change", (e) => {
+        const select = e.target.closest("select");
+        if (!select) return;
+        
+        const book = libraryController.myLibrary.find(book => book.UID === select.dataset.id);
+
+        console.log(libraryController.myLibrary);
+        book.setStatus(select.value);
+    });
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        let item = {
+            title: title.value, 
+            author: author.value, 
+            content: content.value, 
+            pages: pages.value, 
+            status: statuses.value
+        };
+
+        const book = new Book(item);
+        libraryController.addBookToLibrary(book);
+
+        form.reset();
+        popupElement.setAttribute("class", "popup");
+    });
+
+    return {table, form, popupElement};
+})();
+
+const buildDOM = (() => {
+    const addItemRow = (item) => {
+        const row = document.createElement("tr");
+        const cell1 = document.createElement("td");
+        const cell2 = document.createElement("td");
+        const cell3 = document.createElement("td");
+        const cell4 = document.createElement("td");
+        const cell5 = document.createElement("td");
+        const cell6 = document.createElement("td");
+
+        cell1.textContent = item.title;
+        cell2.textContent = item.content;
+        cell3.textContent = item.author;
+        cell4.textContent = item.pages;
+        cell5.innerHTML = `
+            <select data-id="${item.UID}">
+                <option value="">Select a value...</option>
+                <option value="Reading" ${item.status === "Reading" ? "selected" : ""}>Reading</option>
+                <option value="Completed" ${item.status === "Completed" ? "selected" : ""}>Completed</option>
+                <option value="Dropped" ${item.status === "Dropped" ? "selected" : ""}>Dropped</option>
+                <option value="Pending" ${item.status === "Pending" ? "selected" : ""}>Pending</option>
+            </select>
+        `;
+        cell6.innerHTML = `<button class='trash' id='${item.UID}'>🗑️</button>`;
+
+        init.table.appendChild(row);
+        row.append(cell1, cell2, cell3, cell4, cell5, cell6);
+    };
+
+    const showPopup = () => {
+        init.popupElement.setAttribute("class", "popup open");
+    };
+
+    const removeRow = (element) => {
+        element.closest("tr").remove();
+    };
     
-    const book = myLibrary.find(book => book.UID === select.dataset.id);
+    return { addItemRow, showPopup, removeRow };
+})();
 
-    book.setStatus(select.value);
+const libraryController = (() => {
+    let myLibrary = [];
 
-    console.log(myLibrary)
-});
+    const addBookToLibrary = (book) => {
+        myLibrary.push(book);
 
-form.addEventListener("submit", () => {
-    event.preventDefault();
+        buildDOM.addItemRow(book)
+    };
 
-    const book = new Book(title.value, author.value, content.value, pages.value, statuses.value);
-    addBookToLibrary(book);
+    array.forEach(item => {
+        const book = new Book(item);
+        addBookToLibrary(book);
+    });
 
-    form.reset();
-    popupElement.setAttribute("class", "popup");
-});
-
+    return { addBookToLibrary, myLibrary};
+})();
